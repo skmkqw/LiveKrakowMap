d0ffc8baef3e5c98e3284f42f454a3692674fbdd
skmkqw <tkorsakov77@gmail.com>
2023-12-02 16:38:55 +0100

    Fully ready heaeder and adaptive menu

---
+++ web/js/menu_links.js
@@ -0,0 +1,18 @@
+// Получаем все ссылки меню
+const menuLinks = document.querySelectorAll('.menu-link');
+
+// Функция для выделения ссылки при клике
+function handleClick(event) {
+  // Удаляем класс active у всех ссылок меню
+  menuLinks.forEach(link => {
+    link.classList.remove('active');
+  });
+
+  // Добавляем класс active к нажатой ссылке
+  event.target.classList.add('active');
+}
+
+// Добавляем обработчик события click к каждой ссылке меню
+menuLinks.forEach(link => {
+  link.addEventListener('click', handleClick);
+});
